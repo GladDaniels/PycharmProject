1ff05aa1fc6a08be3b76c54093a96b0ba761d87b
GladDaniels <85552765+GladDaniels@users.noreply.github.com>
2021-06-25 12:24:35 +0300

    v1.03

--- obsh.py
+++ obsh.py
@@ -1,8 +1,7 @@
-def obsh(x, y):
+def obsh(x, y): #  это коммент
     array = []
     for i in range(2, min(x, y)+1):
         if x % i == 0 and y % i == 0:
             array.append(i)
     return array
-
 print(obsh(12, 18))
